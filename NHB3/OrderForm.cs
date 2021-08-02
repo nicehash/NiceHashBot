9d8cb67e64365d3ab55144c7fb4678de87acedeb
bl4z <blaz@nicehash.com>
2021-08-02 10:35:51 +0200

    Update OrderForm.cs

--- NHB3/OrderForm.cs
+++ NHB3/OrderForm.cs
@@ -11,7 +11,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace NHB3
 {
